d11f6e2c57822394bd816ff2de2e622dfcfe3c50
Douglas Munro <douglas.munro@epicor.com>
2022-05-03 11:22:01 +0100

    Update for version greater then 2.4.3, to use static logger

--- Model/LogData.php
+++ Model/LogData.php
@@ -6,15 +6,29 @@ class LogData
 {
     public static function log($data, $logFileName = 'zdebug.log')
     {
-        $path = '/var/log/' . $logFileName;
-        $writer = new \Zend\Log\Writer\Stream(BP . $path);
-        $log = new \Laminas\Log\Logger();
-        $log->addWriter($writer);
-        if(is_string($data)){
-            $log->info($data);
-        }else{
-            $log->info(print_r($data, true));
+        if(class_exists(\Zend\Log\Writer\Stream::class) && class_exists(\Laminas\Log\Logger::class)){
+            $path = '/var/log/' . $logFileName;
+            $writer = new \Zend\Log\Writer\Stream(BP . $path);
+            $log = new \Laminas\Log\Logger();
+            $log->addWriter($writer);
+            if(is_string($data)){
+                $log->info($data);
+            }else{
+                $log->info(print_r($data, true));
+            }
+            return true;
         }
+        if(class_exists(\Dev\LogFile::class)){
+            \Dev\LogFile::$config = [
+                'base_path' => BP . '/',
+                'log_file' => $logFileName
+            ];
+            \Dev\LogFile::logFile($data);
+            return true;
+        }
+        $file = BP . '/var/log/' . $logFileName;
+        file_put_contents($file, 'Error: Unable to log data check config', FILE_APPEND);
+
     }
 
     public static function logClass($object, $ref='object')
